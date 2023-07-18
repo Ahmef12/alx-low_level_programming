90a94d20c15733c1dc639af18b2f859c74bd7d30
Ahmef12 <ahmed.abdellatife202020@gmail.com>
2023-07-18 03:46:30 -0700

    n

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,21 @@ void times_table(void)
 				if (j != 0)
 					_putchar(' ');
 				_putchar(n + '0');
-
+					if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
 			}
+		}
+		_putchar('\n');
+	}
+}
